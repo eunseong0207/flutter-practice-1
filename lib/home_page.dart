1081c1e712b6db2484ea91b1a6e74b564c0947de
지은성 <ddrr2852@gmail.com>
2025-12-24 09:27:45 +0900

    feat : 빈페이지

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_1/main.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("블로그 샘플"),
+        //
+      ),
+    );
+  }
+}
